3d8f35643e38b3bffcf95d17fc5a3c37cccf30c0
ian-otieno <ian.otieno@student.moringaschool.com>
2022-04-02 11:55:24 +0300

    class profile to models.py

--- Instagram/models.py
+++ Instagram/models.py
@@ -1,3 +1,34 @@
+from collections import UserDict
 from django.db import models
+from django.db.models.deletion import CASCADE
+from django.db.models.fields import CharField, TextField
+from django.db.models.fields.files import ImageField
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+
 
 # Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.TextField()
+    profile_photo = models.ImageField(upload_to ="images/", default='profile/default.jpg')
+    
+
+    def __str__(self):
+        return self.user.username
+
+    def save_profile(self):
+        self.save()
+
+    def delete_profile(self):
+        self.delete()
+
+    @classmethod
+    def update_profile(cls, value):
+        profile = cls.objects.filter(id = value).update()
+        return profile
+
+    @classmethod
+    def search_profile(cls, name):
+        return cls.objects.filter(user__username__icontains=name).all()
+
