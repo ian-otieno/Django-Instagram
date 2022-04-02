bfc232fada96d27ad38fb779d942554d4e2e74ce
ian-otieno <ian.otieno@student.moringaschool.com>
2022-04-02 12:20:41 +0300

    class index function to views.py

--- Instagram/views.py
+++ Instagram/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.contrib.messages import success
+from django.core.checks import messages
+from django.shortcuts import redirect, render,get_object_or_404
+from django.http import HttpResponse, Http404, HttpResponseRedirect
+from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView
+from .models import Images, Profile, Comment
+from django.views import generic
+from django.urls import reverse_lazy, reverse
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import UserChangeForm
 
 # Create your views here.
+
+@login_required(login_url='/accounts/login/')
+def index(request):
+    name = 'instagram app'
+    images = Images.objects.all()
+    comments = Comment.objects.all()
+    
+    return render(request, 'instagram/index.html', {"name":name, "images":images, "comments":comments})
+
